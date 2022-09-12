ea01bf96f5eb6a963ef75d6b5e76635ebdec7116
Yuriy Zvirblis <ogwhitiez@gmail.com>
2022-09-12 10:33:47 +0300

    quick fix

--- src/actions/botActions.ts
+++ src/actions/botActions.ts
@@ -1,7 +1,6 @@
 import { Composer } from "telegraf";
-const YuraZvirblis1001705288428 = require("./actionLibrary/YuraZvirblis.-1001705288428.actions");
-const Elvis1001791937124 = require("./actionLibrary/Elvis.-1001791937124.actions");
+const YuraZvirblis = require("./actionLibrary/YuraZvirblis.actions");
 
-const actions = [YuraZvirblis1001705288428, Elvis1001791937124];
+const actions = [YuraZvirblis];
 
 module.exports = Composer.action(actions);
